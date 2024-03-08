aaeca06f4996c608cf8dd2317432238f2a6d6c54
YevgenOrlov <101797140+YevgenOrlov@users.noreply.github.com>
2024-03-08 20:52:21 +0200

    80324

--- src/component/Profile/MyPost/MyPosts.jsx
+++ src/component/Profile/MyPost/MyPosts.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import s from "./MyPost.module.css";
 import Post from "./Post/Post";
-import { updateNewPostText } from "../../../redux/state";
+
 
 
 const MyPost = (props) => {
-  debugger
+  
   let postElements= props.posts.map(p=>
   <Post message={p.message} likesCount={p.likesCount}/>)
 
   let newPostElement = React.createRef();
 
   let addPost = () => {   
-    props.addPost(text);  
+  
+
+    props.addPost();  
   }
   
   let onPostChange = () => {
     let text = newPostElement.current.value;
     props.updateNewPostText(text);
+    
  }
+
   return (
     
     <div className={s.postBlock}>
