32ada8d9f795d0b2f627e96ec4e981da56f72220
YevgenOrlov <101797140+YevgenOrlov@users.noreply.github.com>
2024-03-10 20:42:31 +0200

    10324

--- src/component/Dialogs/Dialogs.jsx
+++ src/component/Dialogs/Dialogs.jsx
@@ -12,9 +12,6 @@ const Dialogs = (props) => {
 let dialogsElements = props.state.dialogs.map (d => <DialogItem name={d.name} id={d.id} key={d.id}/>)
 let messagesElements = props.state.messages.map(m => <Message message={m.message} id={m.id} key={m.id}/>)
 
-
-
-
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
